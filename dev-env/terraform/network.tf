06d2555e67235a5457d5fa7cbe43eb437d073c9a
あんでぃー <bboy.answer.d@gmail.com>
2020-04-23 09:44:50 +0900

    add resource to network.tf

--- dev-env/terraform/network.tf
+++ dev-env/terraform/network.tf
@@ -47,9 +47,29 @@ resource "aws_security_group" "public" {
 # todo: modify this vulnerable rule!
 resource "aws_security_group_rule" "in_ssh" {
     security_group_id = aws_security_group.public.id
-    type              = "ingress"
-    cidr_blocks       = ["0.0.0.0/0"]
-    from_port         = 22
-    to_port           = 22
-    protocol          = "tcp"
+    type = "ingress"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+}
+
+# インバウンドルール(pingコマンド用)
+resource "aws_security_group_rule" "in_icmp" {
+    security_group_id = aws_security_group.public.id
+    type = "ingress"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+}
+
+# アウトバウンドルール(全開放)
+resource "aws_security_group_rule" "out_all" {
+    security_group_id = aws_security_group.public.id
+    type = "egress"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
 }
