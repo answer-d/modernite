892b6c59a86154d03a414403a1c92c35d70031c4
あんでぃー <bboy.answer.d@gmail.com>
2020-05-31 02:52:03 +0900

    SSM Session Manager利用するための設定を追加

--- dev-env/terraform/compute.tf
+++ dev-env/terraform/compute.tf
@@ -29,12 +29,46 @@ data "aws_ami" "ami_amzn2" {
   }
 }
 
+data "aws_iam_policy_document" "assume_role_policy_ec2_ssm" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ec2_ssm" {
+  name = "${var.prefix_name}-${var.system_name}-${var.stage}-ec2-ssm"
+  path = "/${var.prefix_name}-${var.system_name}-${var.stage}/"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy_ec2_ssm.json
+
+  tags = {
+    Name = "${var.prefix_name}-${var.system_name}-${var.stage}-ec2-ssm"
+    Author = var.author
+    Stage = var.stage
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_ssm" {
+  role = aws_iam_role.ec2_ssm.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
+
+resource "aws_iam_instance_profile" "ssm" {
+  name = "${var.prefix_name}-${var.system_name}-${var.stage}-ssm"
+  role = aws_iam_role.ec2_ssm.name
+}
+
 resource "aws_instance" "dev" {
   ami = data.aws_ami.ami_amzn2.image_id
   vpc_security_group_ids = [aws_security_group.public.id]
   subnet_id = aws_subnet.public.id
   key_name = var.key_name
   instance_type = var.dev_instance_type
+  iam_instance_profile = aws_iam_instance_profile.ssm.id
 
   tags = {
     Name = "${var.prefix_name}-${var.system_name}-${var.stage}-dev"
